97c84a9d18c007838922897b99b266a853fd648a
PhamHuy <phamkhachuy240702@gmail.com>
2024-03-18 17:31:24 +0700

    update

--- src/layouts/Admin/Dashboard.vue
+++ src/layouts/Admin/Dashboard.vue
@@ -9,7 +9,6 @@
 <script lang="ts" setup>
 import CardItem from '../Admin/CardItem.vue'
 import TableVue from '../Admin/Table.vue'
-import ChartVue from '../Admin/Chart.vue'
 </script>
 
 <style></style>
